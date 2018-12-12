a0b0733253789193a9cc15e055722a3f68a6a57f
Vivek Roy <thezealousfool@gmail.com>
2018-12-12 21:59:16 +0530

    Animated sidenav elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,26 @@
 var nav_exp = '';
+var nav_exp_mh = 0;
 
 function expand(elem_id) {
     const elem = document.getElementById(elem_id);
     if (elem) {
         if (nav_exp === elem_id) {
-            elem.classList.remove('show-ul');
+            elem.style.maxHeight = nav_exp_mh + 'px';
             nav_exp = '';
+            nav_exp_mh = 0;
             return;
         } else if (nav_exp !== '') {
             const exp_elem = document.getElementById(nav_exp);
-            exp_elem.classList.remove('show-ul');
+            exp_elem.style.maxHeight = nav_exp_mh + 'px';
         }
-        elem.classList.add('show-ul');
         nav_exp = elem_id;
+        nav_exp_mh = elem.clientHeight;
+        elem.style.maxHeight = 'none';
+        const elem_h = elem.clientHeight;
+        elem.style.maxHeight = nav_exp_mh + 'px';
+        window.setTimeout(function() {
+            elem.style.maxHeight = elem_h + 'px';
+        }, 5);
     } else {
         console.log(elem_id, "not found");
     }
