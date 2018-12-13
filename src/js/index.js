b152f0d811dad59c53df5a141a12a19e3e2d3bdc
Vivek Roy <thezealousfool@gmail.com>
2018-12-13 21:46:58 +0530

    Changed max-heights to hard-coded 2.2em everywhere

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,20 @@
 var nav_exp = '';
-var nav_exp_mh = 0;
 
 function expand(elem_id) {
     const elem = document.getElementById(elem_id);
     if (elem) {
         if (nav_exp === elem_id) {
-            elem.style.maxHeight = nav_exp_mh + 'px';
+            elem.style.maxHeight = '2.2em';
             nav_exp = '';
-            nav_exp_mh = 0;
             return;
         } else if (nav_exp !== '') {
             const exp_elem = document.getElementById(nav_exp);
-            exp_elem.style.maxHeight = nav_exp_mh + 'px';
+            exp_elem.style.maxHeight = '2.2em';
         }
         nav_exp = elem_id;
-        nav_exp_mh = elem.clientHeight;
         elem.style.maxHeight = 'none';
         const elem_h = elem.clientHeight;
-        elem.style.maxHeight = nav_exp_mh + 'px';
+        elem.style.maxHeight = '2.2em';
         window.setTimeout(function() {
             elem.style.maxHeight = elem_h + 'px';
         }, 5);
