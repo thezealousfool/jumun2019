0c8f411b81fb5f66483605fa76fa9289c13249d4
Vivek Roy <thezealousfool@gmail.com>
2018-12-14 20:52:26 +0530

    Nav max-height fix in js

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,17 @@ function expand(elem_id) {
     const elem = document.getElementById(elem_id);
     if (elem) {
         if (nav_exp === elem_id) {
-            elem.style.maxHeight = '2.2em';
+            elem.style.maxHeight = '2.6em';
             nav_exp = '';
             return;
         } else if (nav_exp !== '') {
             const exp_elem = document.getElementById(nav_exp);
-            exp_elem.style.maxHeight = '2.2em';
+            exp_elem.style.maxHeight = '2.6em';
         }
         nav_exp = elem_id;
         elem.style.maxHeight = 'none';
         const elem_h = elem.clientHeight;
-        elem.style.maxHeight = '2.2em';
+        elem.style.maxHeight = '2.6em';
         window.setTimeout(function() {
             elem.style.maxHeight = elem_h + 'px';
         }, 5);
